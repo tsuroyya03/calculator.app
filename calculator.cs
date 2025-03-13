fa07d6bf60f2e8d3bea2247dec174e4ba880fb8f
ayya <tsuroyyayaa@students.amikom.ac.id>
2025-03-13 11:36:28 +0700

    Melengkapi fungsi Main

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,14 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		console.WriteLine("\nTekan sembarang key untuk keluar");
+		console.ReadKey();
 		
 	}
 	
